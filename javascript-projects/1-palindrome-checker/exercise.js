258390b436092c819eda7c364a95aa4014cc639d
Braedon <braedondavies@gmail.com>
2020-05-13 22:18:10 -0600

    updating files

--- javascript-projects/1-palindrome-checker/exercise.js
+++ javascript-projects/1-palindrome-checker/exercise.js
@@ -1,41 +1,62 @@
 function palindrome(str) {
     console.log("~str: " + str);
-    var originalWord = str;
-    var reversedWord = "";
-    var newChar = "";
-    var wordLength = 0;
-    wordLength = str.length;
-    for (var i = str.length; i > 0; i--) {
-        newChar = str[i - 1];
-        // console.log("newChar: " + newChar);
-        reversedWord = sortNewChar(reversedWord, newChar);
+    // Values
+    var pureStr = str;
+      var reversedWord = "";
+      var newChar = "";
+      var wordLength = 0;
+      var wordMatch = Boolean(false);
+      wordLength = str.length;
+      // Cleans original string
+      pureStr = stringCleaning(pureStr);
+      // Creates reverse spelling
+      for (var i = str.length; i > 0; i--) {
+          newChar = str[i - 1];
+          // console.log("newChar: " + newChar);
+          reversedWord = sortNewChar(reversedWord, newChar);
+      }
+      wordMatch = compareWords(str, reversedWord);
+      console.log(">>> wordMatch: " + wordMatch);
+      return wordMatch;
+  }
+  
+  function stringCleaning(pureStr){
+    for (var i = 0; i < pureStr.length; i++){
+      var letterRange = /[a-z]/g;
+      var matchLetter = pureStr[i].match(letterRange);
+      if (matchLetter == ""){
+        
+      }
     }
-    return compareWords(originalWord, reversedWord);
-}
-
-function sortNewChar(reversedWord, thisChar) {
-    // console.log(":?: reversedWord: " + reversedWord);
-    // console.log(":?: thisChar: " + thisChar);
-    var letterRange = /[a-z]/g;
-    var matchLetter = thisChar.match(letterRange);
-    if (thisChar == matchLetter) {
-        reversedWord = reversedWord + thisChar;
-        // console.log("(+) reversedWord: " + reversedWord);
-    } else {
-        reversedWord = reversedWord;
-        // console.log("(-) reversedWord: " + reversedWord);
-    }
-    return reversedWord;
-}
-
-function compareWords(oldWord, newWord) {
+  }
+  
+  /*
+  FIX THIS
+  */
+  function sortNewChar(changedWord, thisChar) {
+      // console.log(":?: reversedWord: " + reversedWord);
+      // console.log(":?: thisChar: " + thisChar);
+      var letterRange = /[a-z]/g;
+      var matchLetter = thisChar.match(letterRange);
+      if (thisChar == matchLetter) {
+          changedWord = changedWord + thisChar;
+          // console.log("(+) reversedWord: " + reversedWord);
+      } else {
+          changedWord = changedWord;
+          // console.log("(-) reversedWord: " + reversedWord);
+      }
+      return changedWord;
+  }
+  
+  function compareWords(oldWord, newWord) {
     console.log("oldWord: " + oldWord);
     console.log("newWord: " + newWord);
-    if (newWord == oldWord) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-palindrome("eye");
+      if (newWord == oldWord) {
+          return true;
+      } else {
+          return false;
+      }
+  }
+  
+  palindrome("eye");
+  
