d9880e69eeafa3dbc2c13533008f59243a24ebf7
Braedon <braedondavies@gmail.com>
2020-05-14 22:17:41 -0600

    updating files

--- javascript-projects/1-palindrome-checker/exercise.js
+++ javascript-projects/1-palindrome-checker/exercise.js
@@ -1,62 +1,46 @@
 function palindrome(str) {
     console.log("~str: " + str);
     // Values
-    var pureStr = str;
-      var reversedWord = "";
-      var newChar = "";
-      var wordLength = 0;
-      var wordMatch = Boolean(false);
-      wordLength = str.length;
-      // Cleans original string
-      pureStr = stringCleaning(pureStr);
-      // Creates reverse spelling
-      for (var i = str.length; i > 0; i--) {
-          newChar = str[i - 1];
-          // console.log("newChar: " + newChar);
-          reversedWord = sortNewChar(reversedWord, newChar);
-      }
-      wordMatch = compareWords(str, reversedWord);
-      console.log(">>> wordMatch: " + wordMatch);
-      return wordMatch;
-  }
-  
-  function stringCleaning(pureStr){
-    for (var i = 0; i < pureStr.length; i++){
-      var letterRange = /[a-z]/g;
-      var matchLetter = pureStr[i].match(letterRange);
-      if (matchLetter == ""){
-        
-      }
+    var pureStr = "";
+    var reversedWord = "";
+    var newChar = "";
+    var wordMatch = Boolean(false);
+    // Creates "clean" version of original and reversed strings
+    for (var i = 0; i < str.length; i++) {
+        newChar = str[i];
+        console.log("newChar before edit: " + newChar);
+        // uses function to check for letters
+        newChar = stringCleaning(newChar);
+        // adds newChar to new strings
+        pureStr = pureStr + newChar;
+        reversedWord = newChar + reversedWord;
+
     }
-  }
-  
-  /*
-  FIX THIS
-  */
-  function sortNewChar(changedWord, thisChar) {
-      // console.log(":?: reversedWord: " + reversedWord);
-      // console.log(":?: thisChar: " + thisChar);
-      var letterRange = /[a-z]/g;
-      var matchLetter = thisChar.match(letterRange);
-      if (thisChar == matchLetter) {
-          changedWord = changedWord + thisChar;
-          // console.log("(+) reversedWord: " + reversedWord);
-      } else {
-          changedWord = changedWord;
-          // console.log("(-) reversedWord: " + reversedWord);
-      }
-      return changedWord;
-  }
-  
-  function compareWords(oldWord, newWord) {
+    wordMatch = compareWords(pureStr, reversedWord);
+    console.log(">>> wordMatch: " + wordMatch);
+    return wordMatch;
+}
+
+function stringCleaning(thisChar) {
+    var letterRange = /[a-z]/g;
+    var matchLetter = thisChar.match(letterRange);
+    if (matchLetter == thisChar) {
+        // console.log("(+) thisChar: " + thisChar);
+        return thisChar;
+    } else {
+        // console.log("(-) thisChar: " + thisChar);
+        return "";
+    }
+}
+
+function compareWords(oldWord, newWord) {
     console.log("oldWord: " + oldWord);
     console.log("newWord: " + newWord);
-      if (newWord == oldWord) {
-          return true;
-      } else {
-          return false;
-      }
-  }
-  
-  palindrome("eye");
-  
+    if (newWord == oldWord) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+palindrome("eye");
