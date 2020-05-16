c140d41076002778a4e7af8984bb60004523a700
Braedon <braedondavies@gmail.com>
2020-05-15 23:10:32 -0600

    adding/updating files

--- javascript-projects/2-roman-numerals/exercise.js
+++ javascript-projects/2-roman-numerals/exercise.js
@@ -1,53 +1,74 @@
 function convertToRoman(num) {
-    var arabicNumerals = num.toString();
+    // var arabicNumerals = num.toString();
     var arabicDigits = [];
+    var curRomanNumeral = "";
     var romanNumerals = "";
     var nthPower = 0;
-    for (var i = arabicNumerals.length; i > 0; i--){
-      if (arabicNumerals[i] == "0"){
-        console.log(">>> arabicNumerals[i]: " + arabicNumerals[i]);
-        return "";
-      }
+    for (var i = 0; i < arabicNumerals.length; i++){
+      console.log("num: " + num);
+      // if (arabicNumerals[i] == "0"){
+      //   console.log(">>> arabicNumerals[i]: " + arabicNumerals);
+      //   return "";
+      // }
       // 10^0 = 1, 10^1 = 10, 10^2 = 100
-      nthPower = arabicNumerals.length - i;
+      nthPower = i;
       console.log("nthPower: " + nthPower);
       // function for numeral conversion
-      arabicToRoman(arabicNumerals[i], nthPower)
+      curRomanNumeral = arabicToRoman(arabicNumerals[i], nthPower);
+      romanNumerals = romanNumerals + curRomanNumeral;
     }
-   return num;
+   return romanNumerals;
   }
   
   convertToRoman(36);
   
   function arabicToRoman(modernNum, nthPower){
     var newDigit = "";
+    var newRomanNumeral = "";
     if (nthPower == 0){
       newDigit = modernNum;
       // 1-9
-      onesPlace(newDigit);
+      newRomanNumeral = onesPlace(newDigit);
     } else if (nthPower == 1){
       newDigit = modernNum + "0";
       // 10s place
-      tensPlace(newDigit);
+      newRomanNumeral = tensPlace(newDigit);
     } else if (nthPower == 2){
       newDigit = modernNum + "00";
       // 100s place
-      hundredsPlace(newDigit);
+      newRomanNumeral = hundredsPlace(newDigit);
     } else if (nthPower == 3){
       newDigit = modernNum + "000";
       // 1000s place
-      thousandsPlace(newDigit);
+      newRomanNumeral = thousandsPlace(newDigit);
     } else {
       alert("Not a valid number");
     }
+    return newRomanNumeral;
   }
   
   function onesPlace(thisDigit){
     var newRomanNumeral = "";
     if (thisDigit == "1"){
       newRomanNumeral = "I"
-    } else if (thisDigit == "1"){
-      newRomanNumeral = "I"
+    } else if (thisDigit == "2"){
+      newRomanNumeral = "II"
+    } else if (thisDigit == "3"){
+      newRomanNumeral = "III"
+    } else if (thisDigit == "4"){
+      newRomanNumeral = "IV"
+    } else if (thisDigit == "5"){
+      newRomanNumeral = "V"
+    } else if (thisDigit == "6"){
+      newRomanNumeral = "VI"
+    } else if (thisDigit == "7"){
+      newRomanNumeral = "VII"
+    } else if (thisDigit == "8"){
+      newRomanNumeral = "VIII"
+    } else if (thisDigit == "9"){
+      newRomanNumeral = "IX"
+    } else if (thisDigit == "10"){
+      newRomanNumeral = "X"
     }
-  
+    return newRomanNumeral;
   }
