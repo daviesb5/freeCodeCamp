f66c3b6ec32c468244e9df7f603ff7e475b901e6
Braedon <braedondavies@gmail.com>
2020-05-17 20:52:46 -0600

    adding/updating files

--- javascript-projects/3-caesars-cipher/exercise.js
+++ javascript-projects/3-caesars-cipher/exercise.js
@@ -7,83 +7,63 @@ function rot13(str) {
     var cipherArray = [];
     // setArrays(normalArray, cipherArray);
     // cycles through characters
-    for (var i = 0; i < str.length; i++){
-      curChar = str[i];
-      console.log("curChar: " + curChar);
-      newChar = checkForLetters(curChar);
-      console.log("newChar: " + newChar);
+    for (var i = 0; i < str.length; i++) {
+        curChar = str[i];
+        console.log("curChar: " + curChar);
+        newChar = convertChar(curChar);
+        console.log(">>> newChar: " + newChar);
     }
     return str;
-  }
-  
-  /*
-  // Sets arrays to a-z and A-Z
-  function setArrays(normalArray, cipherArray){
-    // variables
-    var numOfLetters = 26;
-    var upperCaseChar = 65; 
-    var upperCaseMax = 90;
-    // var lowerCaseChar = 97;
-    // var lowerCaseMax = 122;
-    // assigns letters
-    for (var i = 0, upperCaseChar = 65; i < numOfLetters; i++, upperCaseChar++){
-      var cipherDif = 13;
-      var cipherChar = upperCaseChar + cipherDif;
-      // stays within ASCII Characters 65-90
-      if (cipherDif <= upperCaseMax){
-        cipherChar = cipherChar;
-      } else {
-        cipherChar = cipherChar - cipherDif;
-      }
-      // adds characters to arrays
-      normalArray[i] = char("&#" + upperCaseChar);
-      console.log("normalArray[i]: " + normalArray[i]);
-    }
-  }
-  */
-  
-  function convertChar(thisChar){
-    // check for A-Z range
-    var isLetter = checkForLetters(thisChar);
-    if (isLetter == true){
-      
-    }
-    // variables
-    var cipherDif = 13;
-    var reroundChar = 26;
-    var upperCaseMin = 65;
-    var upperCaseMax = 90;
-    var oldNumCode = 0;
-    var newNumCode = 0;
+}
+
+function convertChar(thisChar) {
+    var checkThisChar = thisChar;
     var numCode = "";
-    // conversion
-    oldNumCode = thisChar.charCodeAt(0);
-    console.log("oldNumCode: " + oldNumCode);
-    newNumCode = oldNumCode - cipherDif;
-    console.log("newNumCode: " + newNumCode);
-    // stays within ASCII Characters 65-90
-    if (oldNumCode >= upperCaseMin){
-      numCode = numCode;
-      console.log("(+) numCode: " + numCode);
-    } else {
-      numCode = numCode + reroundChar;
-      console.log("(!) numCode: " + numCode);
+    // check for A-Z range
+    var isLetter = Boolean(checkForLetters(checkThisChar));
+    if (isLetter == false) {
+        // console.log("No changes needed.");
+        return thisChar;
     }
-  }
-  
-  function checkForLetters(thisChar){
+    // Get ASCII Key
+    numCode = dialThatCipher(thisChar);
+    console.log("numCode: " + numCode);
+}
+
+function checkForLetters(thisChar) {
     var isTrue = Boolean(false);
     var curChar = thisChar;
     var letterRange = /[A-Z]/g;
     var matchLetter = curChar.match(letterRange);
     // returns true or false
-    if (curChar == matchLetter){
-      isTrue = true;
+    if (curChar == matchLetter) {
+        isTrue = true;
     } else {
-      isTrue = false;
+        isTrue = false;
     }
-    console.log("isTrue: " + isTrue);
-  }
-  
-  rot13("SERR PBQR PNZC");
-  
+    // console.log("isTrue: " + isTrue);
+    return isTrue;
+}
+
+function dialThatCipher(thisChar) {
+    // variables
+    var cipherDif = 13;
+    var reroundChar = 26;
+    var upperCaseMin = 65;
+    var oldNumCode = 0;
+    var newNumCode = 0;
+    // conversion
+    oldNumCode = thisChar.charCodeAt(0);
+    newNumCode = oldNumCode - cipherDif;
+    // Enforces ASCII Character Range: 65-90
+    if (newNumCode >= upperCaseMin) {
+        newNumCode = newNumCode;
+        console.log("(+) newNumCode: " + newNumCode);
+    } else {
+        newNumCode = newNumCode + reroundChar;
+        console.log("(!) newNumCode: " + newNumCode);
+    }
+    return newNumCode;
+}
+
+rot13("SERR PBQR PNZC");
